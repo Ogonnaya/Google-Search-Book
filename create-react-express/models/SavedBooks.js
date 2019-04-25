8ccd615a7581e8a656f373b879b9ac033d64ce1a
Namita Shenai <namita.shenai@gmail.com>
2019-04-25 11:15:58 -0700

    Added database columns for the SavedBooks model

--- create-react-express/models/SavedBooks.js
+++ create-react-express/models/SavedBooks.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const Schema =  mongoose.Schema; 
 
 const bookSchema = new Schema({
-    
+    title: {type: String , required: true},
+    author: {type: String, required: true}, 
+    description: {type: String, required: true}, 
+    href: {type: String, required: true}, 
+    thumbnail: {type:String , required: true}
 }); 
 
 const SavedBooks = mongoose.model("SavedBooks", bookSchema); 
